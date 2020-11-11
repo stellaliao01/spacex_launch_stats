65062d54fa4365eaabfff761fd4eec5057a2a5c9
Stella Liao <stellaliao@Stellas-MacBook-Pro.local>
2020-11-11 02:28:52 -0500

    create graphql server

--- schema.js
+++ schema.js
@@ -1,82 +1,89 @@
-const axios = require('axios');
+//where all the stuff with graphql will go 
+//users, posts, all would be object types 
+const axios = require('axios')
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema} = require('graphql')
 
-const {
-  GraphQLObjectType,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLBoolean,
-  GraphQLList,
-  GraphQLSchema
-} = require('graphql');
+//using 3rd party api
 
-// Launch Type
+//launch type 
 const LaunchType = new GraphQLObjectType({
-  name: 'Launch',
-  fields: () => ({
-    flight_number: { type: GraphQLInt },
-    mission_name: { type: GraphQLString },
-    launch_year: { type: GraphQLString },
-    launch_date_local: { type: GraphQLString },
-    launch_success: { type: GraphQLBoolean },
-    rocket: { type: RocketType }
-  })
-});
-
-// Rocket Type
+    name: 'Launch', 
+    fields: () => ({
+        flight_number: {type: GraphQLInt},
+        mission_name: {type: GraphQLString}, 
+        launch_year: {type: GraphQLString}, 
+        launch_date_local: {type: GraphQLString}, 
+        launch_success: {type: GraphQLBoolean}, 
+        //creating your own type, how you create realationships in your schema
+        rocket: {type: RocketType}
+})
+})
+//rockettype
 const RocketType = new GraphQLObjectType({
-  name: 'Rocket',
-  fields: () => ({
-    rocket_id: { type: GraphQLString },
-    rocket_name: { type: GraphQLString },
-    rocket_type: { type: GraphQLString }
-  })
-});
+    name: 'Rocket', 
+    fields: () => ({
+        rocket_id: {type: GraphQLString},
+        rocket_name: {type: GraphQLString}, 
+        rocket_type: {type: GraphQLString}, 
+})
+})
 
-// Root Query
+//root query 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    launches: {
-      type: new GraphQLList(LaunchType),
-      resolve(parent, args) {
-        return axios
-          .get('https://api.spacexdata.com/v3/launches')
-          .then(res => res.data);
-      }
-    },
-    launch: {
-      type: LaunchType,
-      args: {
-        flight_number: { type: GraphQLInt }
-      },
-      resolve(parent, args) {
-        return axios
-          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
-          .then(res => res.data);
-      }
-    },
-    rockets: {
-      type: new GraphQLList(RocketType),
-      resolve(parent, args) {
-        return axios
-          .get('https://api.spacexdata.com/v3/rockets')
-          .then(res => res.data);
-      }
-    },
-    rocket: {
-      type: RocketType,
-      args: {
-        id: { type: GraphQLInt }
-      },
-      resolve(parent, args) {
-        return axios
-          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
-          .then(res => res.data);
-      }
+    name: 'RootQueryType', 
+    fields: {
+        launches: {
+            //get a list of an array of launches
+            type: new GraphQLList(LaunchType), 
+            resolve(parent, args) {
+                //return the data with the api's end point
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                .then(res => res.data)
+                // .then(data => console.log(data))
+            }
+        }, 
+        launch: {
+            //since we're just getting a single launch we dont need the list
+            type: LaunchType,
+            //the argument to find the launch, like an id so flight number here
+            args: {
+                flight_number: {type: GraphQLInt}
+            }, 
+            resolve(parent, args) {
+                //get request with args.flight_number(the id we're checking for)
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                .then(res => res.data)
+            }
+        }, 
+        rockets: {
+            //get a list of an array of rockets
+            type: new GraphQLList(RocketType), 
+            resolve(parent, args) {
+                //return the data with the api's end point
+                return axios.get('https://api.spacexdata.com/v3/rockets')
+                .then(res => res.data)
+                // .then(data => console.log(data))
+            }
+        }, 
+        rocket: {
+            //since we're just getting a single rocket we dont need the list
+            type: RocketType,
+            //the argument to find the launch, like an id so flight number here
+            args: {
+                id: {type: GraphQLInt
+                i}
+            }, 
+            resolve(parent, args) {
+                //get request with args.id for the specific id we're looking for
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+                .then(res => res.data)
+            }
+        }
     }
-  }
-});
+})
+
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
-});
+    query: RootQuery
+    //if you want mutations to update for crud 
+})
--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
-const schema = require('./schema');
-const path = require('path');
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+// const = require('')
+const schema = require('./schema')
 
-const app = express();
+const app = express()
 
-// Allow cross-origin
-app.use(cors());
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
+//with graphql we only have one endpoint /graphQL 
+//and then a schema 
+app.use('/graphql', graphqlHTTP({
+    schema, 
+    //a useful tool to see everything 
     graphiql: true
-  })
-);
+}))
 
-app.use(express.static('public'));
+const PORT = process.env.port || 5000
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
